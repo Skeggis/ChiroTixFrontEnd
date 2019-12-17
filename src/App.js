8378d129eafb77feff17db5cd3b524f4b5b169b2
Róbert Ingi Huldarsson <rih4@hi.is>
2019-12-17 14:07:23 +0000

    homepage progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {Fragment} from 'react'
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
+import HomePage from './Pages/HomePage/HomePage'
+import 'antd/dist/antd.css';
+import Header from './Components/Header/Header'
 
 
 function App() {
   return (
     <Fragment>
-      
+      <Header/>
+      <Switch>
+        <Route path='/' exact component={HomePage}/>
+      </Switch>
     </Fragment>
   );
 }
