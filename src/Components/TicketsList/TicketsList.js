e71eedda46d078c697d2e0413ef20249fcebbb27
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2019-12-22 18:13:22 +0000

    My commit

--- src/Components/TicketsList/TicketsList.js
+++ src/Components/TicketsList/TicketsList.js
@@ -3,55 +3,64 @@ import { List, Col, Row, Button } from 'antd'
 
 import './TicketsList.scss'
 
-function TicketsList({ ticketTypes = [], location = '', organization = '', onTicket = () => { }, totalTicketPrice=0 }) {
+function TicketsList({ ticketTypes = [], location = '', organization = '', handleTicketChange = () => { }, totalTicketPrice=0, interactive=true }) {
     let theTicketTypes = []
     theTicketTypes.push(
         <List.Item >
             <Row className='TicketsList__listRow'>
                 <Col span={1}></Col>
-                <Col span={6}><h3 className="TicketsList__ticketInfoTitle">Event</h3></Col>
-                <Col span={5}><h3 className="TicketsList__ticketInfoTitle">Organization</h3></Col>
-                <Col span={6}><h3 className="TicketsList__ticketInfoTitle">Location</h3></Col>
-                <Col span={2}><h3 className="TicketsList__ticketInfoTitle">Price</h3></Col>
-                <Col span={4}><h3 className="TicketsList__ticketInfoTitle" style={{ textAlign: "center" }}>Amount</h3></Col>
+                <Col span={4}><h3 className="TicketsList__ticketInfoTitle">Event</h3></Col>
+                <Col span={3}></Col>
+                <Col span={3}><h3 className="TicketsList__ticketInfoTitle rightAlign">Organization</h3></Col>
+                <Col span={2}></Col>
+                <Col span={3}><h3 className="TicketsList__ticketInfoTitle rightAlign">Location</h3></Col>
+                <Col span={1}></Col>
+                <Col span={3}><h3 className="TicketsList__ticketInfoTitle rightAlign">Price</h3></Col>
+                <Col span={2}></Col>
+                <Col span={2}><h3 className="TicketsList__ticketInfoTitle" style={{ textAlign: "center" }}>Amount</h3></Col>
             </Row>
         </List.Item>)
-    ticketTypes.forEach(ticket =>
-        theTicketTypes.push(
-            <List.Item style={{ borderColor: "rgba(0,0,0,0.5)", backgroundColor: "rgb(0,0,0,0.1)" }}>
-                <Row className='TicketsList__listRow'>
-                    <Col span={1}></Col>
-                    <Col span={6} className="TicketsList__ticketTypeInfo"> {ticket.name} </Col>
-                    <Col span={5} className="TicketsList__ticketTypeInfo"> {organization} </Col>
-                    <Col span={6} className="TicketsList__ticketTypeInfo"> {location} </Col>
-                    <Col span={2} className="TicketsList__ticketTypeInfo"> {ticket.price} </Col>
-                    <Col span={4} className="TicketsList__ticketTypeInfo">
-                        <div className="TicketsList__amountDiv">
-                            {/* <div className="TicketsList__roundButton">
-                            <h4>-</h4>
-                        </div> */}
-                            <Button shape="circle" onClick={() => onTicket(ticket.id, false)}>-</Button>
-                            <h3 className="TicketsList__amount">{ticket.amount}</h3>
-                            <Button shape="circle" onClick={() => onTicket(ticket.id, true)}>+</Button>
-                            {/* <div className="TicketsList__roundButton">
-                            <h4>+</h4>
-                        </div> */}
-
-                        </div>
-                    </Col>
-                </Row>
-            </List.Item>)
-    )
+        for(let i = 0; i < ticketTypes.length; i++){
+            let ticket = ticketTypes[i]
+            if(!interactive && ticket.amount === 0){continue}
+            theTicketTypes.push(
+                <List.Item style={{ borderColor: "rgba(0,0,0,0.5)", backgroundColor: "rgb(0,0,0,0.1)" }}>
+                    <Row className='TicketsList__listRow'>
+                        <Col span={1}></Col>
+                        <Col span={4} className="TicketsList__ticketTypeInfo"> {ticket.name} </Col>
+                        <Col span={3}></Col>
+                        <Col span={3} className="TicketsList__ticketTypeInfo rightAlign"> {organization} </Col>
+                        <Col span={2}></Col>
+                        <Col span={3} className="TicketsList__ticketTypeInfo rightAlign"> {location} </Col>
+                        <Col span={1}></Col>
+                        <Col span={3} className="TicketsList__ticketTypeInfo rightAlign"> {ticket.price} </Col>
+                        <Col span={2}></Col>
+                        <Col span={2} className="TicketsList__ticketTypeInfo">
+                            <div className="TicketsList__amountDiv">
+                                {/* <div className="TicketsList__roundButton">
+                                <h4>-</h4>
+                            </div> */}
+                            {interactive ? <Button shape="circle" onClick={() => handleTicketChange(ticket.id, false)} size="small">-</Button>:''}
+                                
+                                <h3 className="TicketsList__amount">{ticket.amount}</h3>
+                                {interactive ? <Button shape="circle" onClick={() => handleTicketChange(ticket.id, true)} size="small">+</Button>:''}
+                                {/* <div className="TicketsList__roundButton">
+                                <h4>+</h4>
+                            </div> */}
+    
+                            </div>
+                        </Col>
+                    </Row>
+                </List.Item>)
+        }
     
     theTicketTypes.push(
         <List.Item >
             <Row className='TicketsList__listRow'>
                 <Col span={1}></Col>
-                <Col span={6}><h3 className="TicketsList__ticketInfoTitle">Total</h3></Col>
-                <Col span={5}></Col>
-                <Col span={6}></Col>
-                <Col span={2}></Col>
-                <Col span={4}><h3 className="TicketsList__ticketInfoTitle" style={{ textAlign: "center" }}>{totalTicketPrice} $</h3></Col>
+                <Col span={4}><h3 className="TicketsList__ticketInfoTitle">Total</h3></Col>
+                <Col span={17}></Col>
+                <Col span={2}><h3 className="TicketsList__ticketInfoTitle" style={{ textAlign: "center" }}>{totalTicketPrice} $</h3></Col>
             </Row>
         </List.Item>)
 
--- src/Pages/TicketsPage/TicketsPage.js
+++ src/Pages/TicketsPage/TicketsPage.js
@@ -1,131 +1,233 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Icon } from 'antd'
 
 import TicketsImage from '../../Components/TicketsImage/TicketsImage'
 import TicketsSteps from '../../Components/TicketsSteps/TicketsSteps'
 
 import TicketsList from '../../Components/TicketsList/TicketsList'
-import BillingInformation from '../../Components/BillingInformation/BillingInformation'
+
+
+import Step2Form from '../../Components/Step2Form/Step2Form'
 import './TicketsPage.scss'
 
 
+let stepsInfo = [{
+    title: "Tickets", subTitle: "00:00:05"
+}, {
+    title: "Billing Information"
+}, {
+    title: "Receipt"
+}]
 function TicketsPage(props) {
-    const data = {
-        image: '../../../tempDefaultImg.jpg',
-        name: 'ChiroPraktik 101',
-        dates: '16.12.19 - 17.12.19',
-        country: 'Germany',
-        city: 'Berlin',
-        organization: 'ICPA',
-        ticketTypes: [{
-            id: 0,
-            name: 'Student',
-            price: 699,
-            amount: 0
-        }, {
-            id: 1,
-            name: 'Chiropraktor',
-            price: 1199,
-            amount: 0
-        }, {
-            id: 2,
-            name: 'Spouse (non chiro)',
-            price: 999,
-            amount: 0
-        }, {
-            id: 3,
-            name: 'Grandpa',
-            price: 3999,
-            amount: 0
-        }]
-    }
-    const [current, setCurrent] = useState(1);
-    const [ticketTypes, setTicketTypes] = useState(data.ticketTypes || []);
-    const [totalTicketPrice, setTotalTicketPrice] = useState(0)
-    const [stepsInfo, setStepsInfo] = useState([{
-        title: "Tickets", subTitle:"00:00:05"
-    }, {
-        title: "Billing Information"
-    }, {
-        title: "Receipt"
-    }])
+    
+    //position of the steps
+    const [current, setCurrent] = useState(0);
 
     
+    const [eventInfo, setEventInfo] = useState({})
+
+    //Array of objects of the types of tickets for this event and with key "amount" representing how many of each type the buyer wants
+    /**
+     * ticketTypes: [{
+     *      id:Integer,
+     *      name: String,
+     *      price: double,
+     *      amount: Integer
+     * }]
+     */
+    const [ticketTypes, setTicketTypes] = useState([]); 
+    //Total price of the tickets
+    const [totalTicketPrice, setTotalTicketPrice] = useState(0) 
     
+    //Array of integer-keys representing which panel of tickets is open (in step 2)
+    const [openPanels, setOpenPanels] = useState([1])
+    const [buyerInfo, setBuyerInfo] = useState({
+        name: '',
+        email: '',
+        confirmEmail: '',
+        phone: '',
+        address: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        agreement: false,
+    })
+    /**
+     * ticketsOwnersInfo: [{
+     *          id: Integer, (the id of the ticketType),
+     *          name: String, (name of the ticketType),
+     *          price: double, 
+     *          header: String, (is set in TOI.js and represents the header-title of its panel)
+     *          extra: boolean or string, (is set in TOI.js and represents wether the panel is to show an icon on the right side (if "empty" then no icon. If true then a success icon, and error icon if false))
+     *          open: boolean, (is set in TOI.js and represents wether this panel is Open)
+     *          ownerInfo: [{
+     *              label: string, (the label-string for the input for this info)
+                    value: string, (the value in the input for this info)
+                    type: string, (the type of input, "checkbox" etc.)
+                    required: boolean, (wether it is required of the user to fill in this info)
+                    isEmpty: boolean (set in TOI.js and represents wether the input for this info is empty or not),
+                    beenTouched: boolean (set in TOI.js and represents wether the input has been in focus in the past, if true the user has clicked it sometime in the past)
+     *          }]
+     * }]
+     */
+    const [ticketsOwnersInfo, setTicketsOwnersInfo] = useState([])
 
 
 
-    let onTicket = (ticketId, add) => {
+    useEffect(() => {
+        const data = {
+            image: '../../../tempDefaultImg.jpg',
+            name: 'ChiroPraktik 101',
+            dates: '16.12.19 - 17.12.19',
+            country: 'Germany',
+            city: 'Berlin',
+            organization: 'ICPA',
+            ticketTypes: [{
+                id: 0,
+                name: 'Student',
+                price: 699
+            }, {
+                id: 1,
+                name: 'Chiropraktor',
+                price: 1199
+            }, {
+                id: 2,
+                name: 'Spouse (non chiro)',
+                price: 999
+            }, {
+                id: 3,
+                name: 'Grandpa',
+                price: 3999
+            }],
+            ownerInfo: [{label:"name", type:"input", required:true}, {label:"Massi", type:"input", required:true}]
+        }
+        setEventInfo(data)
+
+        for(let i = 0; i < data.ticketTypes.length; i++){ data.ticketTypes[i].amount = 0 }
+        setTicketTypes(data.ticketTypes)
+
+    }, [])
+
+
+    let handleTicketChange = (ticketId, addTicket) => {
         let newTickets = JSON.parse(JSON.stringify(ticketTypes))
         for (let i = 0; i < newTickets.length; i++) {
             if (newTickets[i].id === ticketId) {
-                if (add) {
+                if (addTicket) {//Increment the amount of tickets for this type
                     newTickets[i].amount++
                     setTotalTicketPrice(totalTicketPrice + newTickets[i].price)
+                    let ownerInfo = []
+                    for (let k = 0; k < eventInfo.ownerInfo.length; k++) {
+                        let info = eventInfo.ownerInfo[k]
+                        ownerInfo.push({
+                            label: info.label,
+                            value: "",
+                            type: info.type,
+                            required: info.required
+                        })
+                    }
+                    ticketsOwnersInfo.push({
+                        id: newTickets[i].id,
+                        name: newTickets[i].name,
+                        price: newTickets[i].price,
+                        ownerInfo: ownerInfo
+                    })
+                    setTicketsOwnersInfo(ticketsOwnersInfo)
+                    break
                 }
                 else if (newTickets[i].amount > 0) {
                     newTickets[i].amount--
                     setTotalTicketPrice(totalTicketPrice - newTickets[i].price)
+                    ticketsOwnersInfo.reverse()
+                    let ticketToDelete = ticketsOwnersInfo.find(ticket => ticket.id === ticketId)
+                    let index = ticketsOwnersInfo.indexOf(ticketToDelete)
+                    if(index >= 0){ticketsOwnersInfo.splice(index,1)}
+                    ticketsOwnersInfo.reverse()
+                    setTicketsOwnersInfo(ticketsOwnersInfo)
+                    break
                 }
             }
         }
         setTicketTypes(newTickets)
     }
 
-    let onContinue = () => { 
-        if(current === 1) {
-            stepsInfo[2].status = "error"
-            setStepsInfo(stepsInfo)
-        } 
-        setCurrent(current+1) }
+    let stepsController = (direction) => setCurrent(current+direction)
 
-    let onBack = () => { 
-        if(current === 2) {
-            stepsInfo[2].status = ""
-            setStepsInfo(stepsInfo)
+    let onButtonClick = () =>{
+        let list = JSON.parse(JSON.stringify(ticketsOwnersInfo))
+        let open = []
+        for(let i = 0; i < list.length; i++){
+            list[i].extra = true
+            list[i].open = false
+            for(let j = 0; j < list[i].ownerInfo.length; j++){
+                list[i].extra = list[i].ownerInfo[j].success 
+                if(!list[i].extra){list[i].open = true}
+            }
+            if(!list[i].extra){open.push(i+1)}
         }
-        setCurrent(current - 1)}
+        setOpenPanels(open)
+        setTicketsOwnersInfo(list)
+    }
 
     let continueButton;
-    
-    if(current < 2){
-        continueButton = (<div className="TicketsPage__buttonDiv">
-        <Button onClick={onContinue} className="TicketsPage__button">
-            Find tickets
-        <Icon type="arrow-right" />
-        </Button>
-        </div>);
+    let buyTicketsButton;
+
+    if (current < 2) {
+        if (current === 1) {
+            buyTicketsButton = (
+                <div className="TicketsPage__buttonDiv">
+                    <Button htmlType="submit" form="billingInformationForm" className="TicketsPage__button" onClick={onButtonClick}>
+                        Buy tickets
+                        <Icon type="arrow-right" />
+                    </Button>
+                </div>
+            );
+            continueButton = ""
+        } else {
+            continueButton = (<div className="TicketsPage__buttonDiv">
+                <Button onClick={() => stepsController(1)} className="TicketsPage__button">
+                    Find tickets
+                    <Icon type="arrow-right" />
+                </Button>
+            </div>);
+            buyTicketsButton = ""
+        }
     }
 
     let backButton;
 
-    if(current === 1 || current === 2){
+    if (current === 1 || current === 2) {
         backButton = (<div className="TicketsPage__buttonDiv">
-        <Button onClick={onBack} className="TicketsPage__button">
-        <Icon type="arrow-left" />
-            Back
+            <Button onClick={() => stepsController(-1)} className="TicketsPage__button">
+                <Icon type="arrow-left" />
+                Back
         </Button>
         </div>);
     }
-    
+
 
 
     let componentToShow;
-    if(current === 0){
-        componentToShow = <TicketsList ticketTypes={ticketTypes} organization={data.organization}
-        location={`${data.city}, ${data.country}`} onTicket={onTicket} totalTicketPrice={totalTicketPrice} />
-    } else if(current === 1){
-        componentToShow = <BillingInformation />
+    if (current === 0) {
+        componentToShow = <TicketsList ticketTypes={ticketTypes} organization={eventInfo.organization}
+            location={`${eventInfo.city}, ${eventInfo.country}`} handleTicketChange={handleTicketChange} totalTicketPrice={totalTicketPrice} />
+    } else if (current === 1) {
+        componentToShow = <div className="TicketsPage__billingInfo">
+            <Step2Form 
+            openPanels={openPanels} setOpenPanels={setOpenPanels} 
+            ticketsOwnersInfo={ticketsOwnersInfo} setTicketsOwnersInfo={setTicketsOwnersInfo}
+            buyerInfo={buyerInfo} setBuyerInfo={setBuyerInfo}/>
+            </div> 
     }
 
     return (
         <div className="TicketsPage">
             <div className="TicketsPage__ticketsImage">
-                <TicketsImage imageUrl={data.image} title={data.name} subTitle={data.dates} />
+                <TicketsImage imageUrl={eventInfo.image} title={eventInfo.name} subTitle={eventInfo.dates} />
             </div>
             <div className="TicketsPage__page">
                 <div className="TicketsPage__ticketsSteps">
-                    <TicketsSteps current={current}  stepsInfo={stepsInfo} />
+                    <TicketsSteps current={current} stepsInfo={stepsInfo} />
                 </div>
                 <div >
                     {componentToShow}
@@ -133,6 +235,7 @@ function TicketsPage(props) {
             </div>
             {backButton}
             {continueButton}
+            {buyTicketsButton}
 
         </div>
 
